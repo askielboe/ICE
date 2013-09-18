e7c7ab94c9cd5b5ec758e7349351c44cdc2833f6
Andreas Skielboe <skielboe@gmail.com>
2013-09-18 10:33:43 -0700

    Clean up code
    Removed old out-commented code originally put in place to test the use
    of multiprocessing for adding stuff to the database.
    
    It didn't work, and will need a complete rework if it is to be used
    in the future.

--- load_data.py
+++ load_data.py
@@ -15,7 +15,6 @@
 #--------------------------------------------------------------------------------
 maxNumberOfBcgs = 100000000
 maxNumberOfGalaxies = 100000000
-nWorkers = 20
 
 #--------------------------------------------------------------------------------
 # Create a session
@@ -41,32 +40,16 @@ inFileRedmapper = pyfits.open('data/dr8_redmapper_v3.4_nordcenter_catalog.fit')
 dataRedmapperCGs = inFileRedmapper[1].data
 
 #--------------------------------------------------------------------------------
-# Declare classes and commit them to database
+# Load BCGs
 #--------------------------------------------------------------------------------
-from classes import Redmapper, SDSSGalaxy
-counter = 0
-
-# # Declare sessions
-# sessions = []
-# for i in range(nWorkers):
-# 	session = create_session()
-# 	sessions.append(session)
 from classes import Redmapper
-def appendToDatabase(data_in):
-	for RA, DEC, BCG_SPEC_Z, P_BCG, LAMBDA_CHISQ in data_in:
-		bcg = Redmapper(RA, DEC, BCG_SPEC_Z)
-		bcg.pBCG = P_BCG
-		bcg.lambda_chisq = LAMBDA_CHISQ
-	session.add(bcg)
 
-#import multiprocessing
-#jobs = []
-#from multiprocessing import Pool
-#po = Pool(processes=nWorkers)
-
-n = 0
-data = []
-for MEM_MATCH_ID,RA,DEC,MODEL_MAG,MODEL_MAGERR,IMAG,IMAG_ERR,ZRED,ZRED_E,BCG_SPEC_Z,Z_SPEC_INIT,Z_INIT,Z,LAMBDA_CHISQ,LAMBDA_CHISQ_E,SCALEVAL,MASKFRAC,C_LAMBDA,C_LAMBDA_ERR,Z_LAMBDA,Z_LAMBDA_E,LNLAMLIKE,LNBCGLIKE,LNLIKE,RA_ORIG,DEC_ORIG,P_BCG in dataRedmapperCGs:
+counter = 0
+for MEM_MATCH_ID,RA,DEC,MODEL_MAG,MODEL_MAGERR,IMAG, \
+	IMAG_ERR,ZRED,ZRED_E,BCG_SPEC_Z,Z_SPEC_INIT,Z_INIT, \
+	Z,LAMBDA_CHISQ,LAMBDA_CHISQ_E,SCALEVAL,MASKFRAC, \
+	C_LAMBDA,C_LAMBDA_ERR,Z_LAMBDA,Z_LAMBDA_E,LNLAMLIKE, \
+	LNBCGLIKE,LNLIKE,RA_ORIG,DEC_ORIG,P_BCG in dataRedmapperCGs:
 
 	if (BCG_SPEC_Z == -1.): continue
 
@@ -74,42 +57,10 @@ for MEM_MATCH_ID,RA,DEC,MODEL_MAG,MODEL_MAGERR,IMAG,IMAG_ERR,ZRED,ZRED_E,BCG_SPE
 	bcg.mem_match_id = MEM_MATCH_ID
 	bcg.pBCG = P_BCG
 	bcg.lambda_chisq = LAMBDA_CHISQ
+
 	session.add(bcg)
-
-	#data.append([RA, DEC, BCG_SPEC_Z, P_BCG, LAMBDA_CHISQ])
-
-	#po.apply_async(appendToDatabase, args=(RA, DEC, BCG_SPEC_Z, P_BCG, LAMBDA_CHISQ))
-
-	#result = po.apply_async(appendToDatabase, args=(RA, DEC, BCG_SPEC_Z, P_BCG, LAMBDA_CHISQ))
-	#bcg = result.get()
-	#print bcg
-	#session.add(bcg)
-
-
-
-	# if n == nWorkers:
-	# 	bcgs = po.starmap(appendToDatabase, data)
-	# 	data = []
-	# n += 1
-	#import multiprocessing
-
-	#p = multiprocessing.Process(target=appendToDatabase, args=(RA, DEC, BCG_SPEC_Z, P_BCG, LAMBDA_CHISQ))
-	#p.start()
-
-	#bcg = Redmapper(RA, DEC, BCG_SPEC_Z)
-	#bcg.pBCG = P_BCG
-	#bcg.lambda_chisq = LAMBDA_CHISQ
-
-	#session.add(bcg)
-
-	# if n == nWorkers:
-	# 	n = 0
-	# 	#print data
-	# 	for i in range(nWorkers):
-	# 		po.apply_async(appendToDatabase, args=(data[i]))
-	# 	data = []
-
 	counter += 1
+
 	if (counter == maxNumberOfBcgs): break
 	if (counter % 100 == 0):
 		print(counter , " # - comitting bcgs to database...")
@@ -118,6 +69,10 @@ for MEM_MATCH_ID,RA,DEC,MODEL_MAG,MODEL_MAGERR,IMAG,IMAG_ERR,ZRED,ZRED_E,BCG_SPE
 inFileSDSS = pyfits.open('data/SDSS_DR8_galaxies.fit')
 dataSDSSGalaxies = inFileSDSS[1].data
 
+#--------------------------------------------------------------------------------
+# Load galaxies
+#--------------------------------------------------------------------------------
+from classes import SDSSGalaxy
 # inFileSDSSMags = pyfits.open('data/SDSS_DR8_galaxies_magnitudes.fit')
 # dataSDSSGalaxiesMags = inFileSDSSMags[1].data
 
